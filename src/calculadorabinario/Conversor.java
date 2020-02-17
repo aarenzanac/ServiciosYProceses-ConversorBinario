c4e95fc5334aff3ae02aa5cd993ce8e34afe1c65
aarenzanac <aarenzana@gmail.com>
2020-02-17 08:35:34 +0100

    Conversor de binario para Programación de Servicios y procesos

--- src/calculadorabinario/Conversor.java
+++ src/calculadorabinario/Conversor.java
@@ -5,17 +5,61 @@
  */
 package calculadorabinario;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
 /**
  *
  * @author USUARIO
  */
 public class Conversor {
+    
+   
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String entrada;
+
+	String binario = "", binario2 = "";
+
+	int numint, resto;
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	try {
+
+            entrada = br.readLine();
+
+            numint = Integer.parseInt(entrada);
+
+            while (numint >= 2) {
+		resto = numint % 2;
+		binario2 += resto;
+		numint /= 2;
+
+            }
+
+            binario2 += numint;
+
+            char[] binlist = binario2.toCharArray();
+
+            for (int i = binlist.length-1; i >= 0; i--) {
+
+                binario += binlist[i];
+            }
+
+            System.out.println(binario);
+	} catch (IOException e) {
+
+            e.printStackTrace();
+
+	}
+
     }
-    
+
 }
--- src/calculadorabinario/Principal.java
+++ src/calculadorabinario/Principal.java
@@ -5,17 +5,61 @@
  */
 package calculadorabinario;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import pideDatos.PideDatos;
 /**
  *
  * @author USUARIO
  */
 public class Principal {
 
+    private static final String rutaProyecto = System.getProperty("user.dir");
+    private static final String separador = File.separator;
+    private static final String directorioInterno = "src" + separador + "calculadorabinario";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String binario = "";
+
+            try {
+
+		Process conversor = new ProcessBuilder("java", "-jar",rutaProyecto + separador + directorioInterno + separador + "Conversor.jar").start();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(conversor.getInputStream()));
+
+		PrintStream ps = new PrintStream(conversor.getOutputStream(), true);
+
+		int num = PideDatos.pideEntero("¿qué número quiere transformar a binario?");
+
+		ps.println(Math.abs(num));
+
+		binario = br.readLine();
+
+		if (num >= 0) {
+
+			System.out.println("En binario es:\n"+binario);
+
+		} else {
+
+			System.out.println("En binario es:\n- " + binario);/*por no complicar el programa, 
+
+				añado un signo, pero habría que hacer un complemento a 1 o 2 y limitar el orden
+
+				 de los números enteros a transformar*/
+
+		}
+
+	} catch (IOException e) {
+
+		// TODO Auto-generated catch block
+
+		e.printStackTrace();
+
+	}   
     }
-    
 }
